ddb8a5d9838199b00bd5eb360a32c966b590ecfc
Michael W. Reimann <michael.reimann@openbraininstitute.org>
2026-02-18 10:24:40 +0100

    Wrapped get_token to try and use environmet variable first.

---
+++ src/obi_notebook/get_token.py
@@ -0,0 +1,26 @@
+"""A wrapper for get_token from obi_auth that tries to use an environment variable first."""
+
+from os import getenv
+from time import time
+
+from jwt.exceptions import DecodeError
+from obi_auth import get_token as auth_get_token
+from obi_auth import get_token_info
+
+MIN_TIME_LEFT_SEC = 600
+
+
+def get_token():
+    """Gets an access token for the user."""
+    token = getenv("OBI_ACCESS_TOKEN")
+    if token is None:
+        return auth_get_token()
+
+    try:
+        token_info = get_token_info(token)
+    except DecodeError:
+        return auth_get_token()
+
+    if (token_info["exp"] - time()) <= MIN_TIME_LEFT_SEC:
+        return auth_get_token()
+    return token
